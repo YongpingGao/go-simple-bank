6e09051189aea45d3bee0e5c3e2c6ec9b5435221
ypgao <gaoyongping1990@gmail.com>
2021-11-16 22:04:08 -0800

    fix nil pointer for db

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -19,7 +19,7 @@ var testQueries *Queries
 
 func TestMain(m *testing.M) {
 	var err error
-	testDB, err := sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(dbDriver, dbSource)
 
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
